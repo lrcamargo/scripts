e58efbefc9a77f54d8c02b1e9c4cc078531f7856
Letícia <38875003+lrcamargo@users.noreply.github.com>
2020-05-09 20:13:08 -0300

    Update manutencao.bat
    Adicionadas opções limpar disco e corrigir sistema

--- manutencao.bat
+++ manutencao.bat
@@ -2,17 +2,24 @@
 echo Script Manutencao
 echo Created by: Leticia
 echo Creation Date: 26/04/20
+echo Updated Date: 09/05/20
+title Manutencao
+color 6
 :menu
 echo -------------------------------------------------
 echo Menu Principal
 echo -------------------------------------------------
 echo 1 - Limpar Cache
-echo 2 - Escanear sistema
-echo 3 - Sair
+echo 2 - Limpar Disco
+echo 3 - Escanear sistema
+echo 4 - Corrigir sistema - Win 8 em diante
+echo 5 - Sair
 set /p Input=Escolha a opcao do menu:
 if %Input%==1 goto limpacache
-if %Input%==2 goto escanear
-if %Input%==3 goto sair
+if %Input%==2 goto limpadisco
+if %Input%==3 goto escanear
+if %Input%==4 goto corrigir
+if %Input%==5 goto sair
 
 :limpacache
 cls
@@ -24,41 +31,41 @@ set profiles=%cd%
 for /f "tokens=* delims= " %%u in ('dir /b/ad') do (
 
 cls
-title Deletando %%u COOKIES. . .
+title Deletando %%u COOKIES...
 if exist "%profiles%\%%u\cookies" echo Deletando....
 if exist "%profiles%\%%u\cookies" cd "%profiles%\%%u\cookies"
 if exist "%profiles%\%%u\cookies" del *.* /F /S /Q /A: R /A: H /A: A
 
 cls
-title Deletando %%u Temp Files. . .
+title Deletando %%u Arquivos Temporarios...
 if exist "%profiles%\%%u\Local Settings\Temp" echo Deletando....
 if exist "%profiles%\%%u\Local Settings\Temp" cd "%profiles%\%%u\Local Settings\Temp"
 if exist "%profiles%\%%u\Local Settings\Temp" del *.* /F /S /Q /A: R /A: H /A: A
 if exist "%profiles%\%%u\Local Settings\Temp" rmdir /s /q "%profiles%\%%u\Local Settings\Temp"
 
 cls
-title Deletando %%u Temp Files. . .
+title Deletando %%u Temp Files...
 if exist "%profiles%\%%u\AppData\Local\Temp" echo Deletando....
 if exist "%profiles%\%%u\AppData\Local\Temp" cd "%profiles%\%%u\AppData\Local\Temp"
 if exist "%profiles%\%%u\AppData\Local\Temp" del *.* /F /S /Q /A: R /A: H /A: A
 if exist "%profiles%\%%u\AppData\Local\Temp" rmdir /s /q "%profiles%\%%u\AppData\Local\Temp"
 
 cls
-title Deletando %%u Temporary Internet Files. . .
+title Deletando %%u Temporary Internet Files...
 if exist "%profiles%\%%u\Local Settings\Temporary Internet Files" echo Deletando....
 if exist "%profiles%\%%u\Local Settings\Temporary Internet Files" cd "%profiles%\%%u\Local Settings\Temporary Internet Files"
 if exist "%profiles%\%%u\Local Settings\Temporary Internet Files" del *.* /F /S /Q /A: R /A: H /A: A
 if exist "%profiles%\%%u\Local Settings\Temporary Internet Files" rmdir /s /q "%profiles%\%%u\Local Settings\Temporary Internet Files"
 
 cls
-title Deletando %%u Temporary Internet Files. . .
+title Deletando %%u Temporary Internet Files...
 if exist "%profiles%\%%u\AppData\Local\Microsoft\Windows\Temporary Internet Files" echo Deletando....
 if exist "%profiles%\%%u\AppData\Local\Microsoft\Windows\Temporary Internet Files" cd "%profiles%\%%u\AppData\Local\Microsoft\Windows\Temporary Internet Files"
 if exist "%profiles%\%%u\AppData\Local\Microsoft\Windows\Temporary Internet Files" del *.* /F /S /Q /A: R /A: H /A: A
 if exist "%profiles%\%%u\AppData\Local\Microsoft\Windows\Temporary Internet Files" rmdir /s /q "%profiles%\%%u\AppData\Local\Microsoft\Windows\Temporary Internet Files"
 
 cls
-title Deletando %%u WER Files. . .
+title Deletando %%u WER Files...
 if exist "%profiles%\%%u\AppData\Local\Microsoft\Windows\WER\ReportArchive" echo Deletando....
 if exist "%profiles%\%%u\AppData\Local\Microsoft\Windows\WER\ReportArchive" cd "%profiles%\%%u\AppData\Local\Microsoft\Windows\WER\ReportArchive"
 if exist "%profiles%\%%u\AppData\Local\Microsoft\Windows\WER\ReportArchive" del *.* /F /S /Q /A: R /A: H /A: A
@@ -66,56 +73,56 @@ if exist "%profiles%\%%u\AppData\Local\Microsoft\Windows\WER\ReportArchive" rmdi
 
 
 cls
-title Deletando %Systemroot%\Temp
+title Deletando %Systemroot%\Temp...
 if exist "%Systemroot%\Temp" echo Deletando....
 if exist "%Systemroot%\Temp" cd "%Systemroot%\Temp"
 if exist "%Systemroot%\Temp" del *.* /F /S /Q /A: R /A: H /A: A
 if exist "%Systemroot%\Temp" rmdir /s /q "%Systemroot%\Temp"
 
 cls
-title Deletando %SYSTEMDRIVE%\Temp
+title Deletando %SYSTEMDRIVE%\Temp...
 if exist "%SYSTEMDRIVE%\Temp" echo Deletando....
 if exist "%SYSTEMDRIVE%\Temp" cd "%SYSTEMDRIVE%\Temp"
 if exist "%SYSTEMDRIVE%\Temp" del *.* /F /S /Q /A: R /A: H /A: A
 if exist "%SYSTEMDRIVE%\Temp" rmdir /s /q "%Systemroot%\Temp"
 
 cls
-title Deletando %%u FIREFOX TEMP. . .
+title Deletando %%u FIREFOX TEMP...
 if exist "%profiles%\%%u\AppData\Local\Mozilla\Firefox\Profiles" echo Deletando....
 if exist "%profiles%\%%u\AppData\Local\Mozilla\Firefox\Profiles" cd "%profiles%\%%u\AppData\Local\Mozilla\Firefox\Profiles"
 if exist "%profiles%\%%u\AppData\Local\Mozilla\Firefox\Profiles" del *.* /F /S /Q /A: R /A: H /A: A
 if exist "%profiles%\%%u\AppData\Local\Mozilla\Firefox\Profiles" rmdir /s /q "%profiles%\%%u\AppData\Local\Mozilla\Firefox\Profiles"
 
 cls
-title Deletando %%u CHROME TEMP. . .
+title Deletando %%u CHROME TEMP...
 if exist "%profiles%\%%u\AppData\Local\Google\Chrome\User Data\Default\Cache" echo Deletando....
 if exist "%profiles%\%%u\AppData\Local\Google\Chrome\User Data\Default\Cache" cd "%profiles%\%%u\AppData\Local\Google\Chrome\User Data\Default\Cache"
 if exist "%profiles%\%%u\AppData\Local\Google\Chrome\User Data\Default\Cache" del *.* /F /S /Q /A: R /A: H /A: A
 if exist "%profiles%\%%u\AppData\Local\Google\Chrome\User Data\Default\Cache" rmdir /s /q "%profiles%\%%u\AppData\Local\Google\Chrome\User Data\Default\Cache"
 
 cls
-title Deletando %%u EDGE TEMP. . .
+title Deletando %%u EDGE TEMP...
 if exist "%profiles%\%%u\AppData\Local\Microsoft\Windows\INetCache" echo Deletando....
 if exist "%profiles%\%%u\AppData\Local\Microsoft\Windows\INetCache" cd "%profiles%\%%u\AppData\Local\Microsoft\Windows\INetCache"
 if exist "%profiles%\%%u\AppData\Local\Microsoft\Windows\INetCache" del *.* /F /S /Q /A: R /A: H /A: A
 if exist "%profiles%\%%u\AppData\Local\Microsoft\Windows\INetCache" rmdir /s /q "%profiles%\%%u\AppData\Local\Microsoft\Windows\INetCache"
 
 cls
-title Deletando %%u EDGE COOKIES. . .
+title Deletando %%u EDGE COOKIES...
 if exist "%profiles%\%%u\AppData\Local\Microsoft\Windows\INetCookies" echo Deletando....
 if exist "%profiles%\%%u\AppData\Local\Microsoft\Windows\INetCookies" cd "%profiles%\%%u\AppData\Local\Microsoft\Windows\INetCookies"
 if exist "%profiles%\%%u\AppData\Local\Microsoft\Windows\INetCookies" del *.* /F /S /Q /A: R /A: H /A: A
 if exist "%profiles%\%%u\AppData\Local\Microsoft\Windows\INetCookies" rmdir /s /q "%profiles%\%%u\AppData\Local\Microsoft\Windows\INetCookies"
 
 cls
-title Deletando %%u RDP TEMP. . .
+title Deletando %%u RDP TEMP...
 if exist "%profiles%\%%u\AppData\Local\Microsoft\Terminal Server Client\Cache" echo Deletando....
 if exist "%profiles%\%%u\AppData\Local\Microsoft\Terminal Server Client\Cache" cd "%profiles%\%%u\AppData\Local\Microsoft\Terminal Server Client\Cache"
 if exist "%profiles%\%%u\AppData\Local\Microsoft\Terminal Server Client\Cache" del *.* /F /S /Q /A: R /A: H /A: A
 if exist "%profiles%\%%u\AppData\Local\Microsoft\Terminal Server Client\Cache" rmdir /s /q "%profiles%\%%u\AppData\Local\Microsoft\Terminal Server Client\Cache"
 
 cls
-title Deletando %%u OPERA TEMP. . .
+title Deletando %%u OPERA TEMP...
 if exist "%profiles%\%%u\AppData\Local\Opera Software\Opera Next\Cache" echo Deletando....
 if exist "%profiles%\%%u\AppData\Local\Opera Software\Opera Next\Cache" cd "%profiles%\%%u\AppData\Local\Opera Software\Opera Next\Cache"
 if exist "%profiles%\%%u\AppData\Local\Opera Software\Opera Next\Caches" del *.* /F /S /Q /A: R /A: H /A: A
@@ -123,7 +130,7 @@ if exist "%profiles%\%%u\AppData\Local\Opera Software\Opera Next\Cache" rmdir /s
 
 
 cls
-title Deletando %%u VIVALDI TEMP. . .
+title Deletando %%u VIVALDI TEMP...
 if exist "%profiles%\%%u\AppData\Local\Vivaldi\User Data\Default\Cache" echo Deletando....
 if exist "%profiles%\%%u\AppData\Local\Vivaldi\User Data\Default\Cache" cd "%profiles%\%%u\AppData\Local\Vivaldi\User Data\Default\Cache"
 if exist "%profiles%\%%u\AppData\Local\Vivaldi\User Data\Default\Cache" del *.* /F /S /Q /A: R /A: H /A: A
@@ -132,11 +139,23 @@ if exist "%profiles%\%%u\AppData\Local\Vivaldi\User Data\Default\Cache" rmdir /s
 )
 
 cls
+title Manutencao
 pause
+goto menu
+
+:limpadisco
+@echo on
+c:\windows\SYSTEM32\cleanmgr.exe /dC:
+pause
+@echo off
 goto menu
 
 :escanear
 sfc /scannow
+goto menu
+
+:corrigir
+DISM /Online /Cleanup-Image /RestoreHealth
 goto menu
 
 :sair
