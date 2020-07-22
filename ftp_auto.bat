3c57033955b59f5de6eeaae977a640f87ff8d9eb
Letícia <38875003+lrcamargo@users.noreply.github.com>
2020-07-22 16:08:27 -0300

    Create ftp_auto.bat
    Criado arquivo para fazer download de FTP remoto automaticamente

---
+++ ftp_auto.bat
@@ -0,0 +1,8 @@
+REM é possível usar também com SFTP
+open ftp://user:password@server
+REM baixar arquivo
+get <filename>
+REM remover arquivo remoto
+rm <filename>
+exit
+move <filename> <novo_diretorio>
