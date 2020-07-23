fd988366bf6b0fd0980970aafb1cb4b6584cc7e0
Letícia <38875003+lrcamargo@users.noreply.github.com>
2020-07-23 18:27:13 -0300

    Update ftp_auto.bat
    Atualizado script para alterar permissão do arquivo e fazer download já na pasta desejada.

--- ftp_auto.bat
+++ ftp_auto.bat
@@ -1,8 +1,11 @@
 REM é possível usar também com SFTP
 open ftp://user:password@server
+cd <pasta>
 REM baixar arquivo
-get <filename>
+get <filename> <pasta_destino>
 REM remover arquivo remoto
 rm <filename>
 exit
-move <filename> <novo_diretorio>
+REM concede permissão total no arquivo baixado
+cacls <caminho_arquivo> /E /G everyone:F
+
